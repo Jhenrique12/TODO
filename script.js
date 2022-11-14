8f0d54acec0dac9a88455b6d47a61f8dd0305423
Jhenrique12 <jonhenrique007@gmail.com>
2022-11-13 22:03:22 -0300

    Add: updateLocalStorage function and save todo elements when update the page

--- script.js
+++ script.js
@@ -2,27 +2,68 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todoList = document.getElementById("todoList");
 
+const arrayTodo = JSON.parse(localStorage.getItem("arrayTodo"));
+
+if (arrayTodo) {
+  arrayTodo.forEach((todo) => {
+    addTodoElements(todo);
+  });
+}
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const todoValue = input.value;
+  addTodoElements();
+});
+
+function addTodoElements(todo) {
+  let todoValue = input.value;
+
+  if (todo) {
+    todoValue = todo.text;
+  }
 
   if (todoValue) {
     const todoElement = document.createElement("li");
+
+    if (todo && todo.completed) {
+      todoElement.classList.add("completed");
+    }
+
     todoElement.innerText = todoValue;
 
     todoElement.addEventListener("click", () => {
       todoElement.classList.toggle("completed");
+
+      updateLocalStorage();
     });
 
     todoElement.addEventListener("contextmenu", (e) => {
       e.preventDefault();
 
       todoElement.remove();
+
+      updateLocalStorage();
     });
 
     todoList.appendChild(todoElement);
 
     input.value = "";
+
+    updateLocalStorage();
   }
-});
+}
+
+function updateLocalStorage() {
+  const todoElements = document.querySelectorAll("li");
+
+  const arrayTodo = [];
+
+  todoElements.forEach((todoElement) => {
+    arrayTodo.push({
+      text: todoElement.innerText,
+      completed: todoElement.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("arrayTodo", JSON.stringify(arrayTodo));
+}
